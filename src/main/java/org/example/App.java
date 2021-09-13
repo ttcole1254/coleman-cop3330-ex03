80952b0d09c67b8c9784f134d66142bf2ac389e4
Tyler Coleman <ttcole1254@gmail.com>
2021-09-12 21:10:53 -0400

    Added assignment code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
 
-/**
- * Hello world!
- *
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Tyler Coleman
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner askQuote = new Scanner(System.in);
+        System.out.println("What is the quote?");
+
+        String inputQuote = askQuote.nextLine();
+        if(inputQuote.isEmpty()) {
+            System.out.println("A quote is required!");
+        }
+        else {
+            Scanner askAuthor = new Scanner(System.in);
+            System.out.println("Who said it?");
+            String inputAuthor = askAuthor.nextLine();
+            if(inputAuthor.isEmpty()) {
+                System.out.println("Well someone had to have said it, please enter an author!");
+            }
+            else {
+                System.out.println(inputAuthor + " says, \"" + inputQuote + "\"");
+            }
+        }
     }
 }
